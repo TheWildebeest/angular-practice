f76ed1ba15a6ac9023500cdb72e82ff3ca737869
David Wildman <59884105+TheWildebeest@users.noreply.github.com>
2020-09-16 13:48:50 +0100

    created static data file

---
+++ src/assets/moviedata.ts
@@ -0,0 +1,16 @@
+
+export interface IMovie {
+  title: string;
+  lineOne: string;
+  lineTwo: string;
+  image: string;
+  altText: string;
+}
+
+export const jawsFilm: IMovie = {
+  title: 'CARDYBOI.',
+  lineOne: 'HEYYYYY!',
+  lineTwo: 'A-DIDDLE-DEEEDLE-DADDLEEEEEE!',
+  image: 'assets/images/stanley.jpg',
+  altText: 'picture of stanley'
+};
